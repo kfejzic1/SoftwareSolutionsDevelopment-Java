b133d83eb1422c2d3cfffad10fe2bb9367cb645c
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-11-22 10:03:31 +0100

    Dodana klasa Studij

---
+++ project/Studij.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Studij {
+    private HashMap<Student, Semestar> upisaniStudenti;
+
+    void upisiStudentaNaSemestar(Student student, Semestar semestar){
+        //dodaje u hashmapu key student i value semestar
+    }
+
+    void dajStudenteNaPredmetu(Predmet predmet){
+        //Ispisuje studente na predmetu
+        //Pristupa predmetu preko value hashmape, zatim uzima predmet iz semestra
+    }
+}
