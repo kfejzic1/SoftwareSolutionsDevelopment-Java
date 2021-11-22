2c760d6c552e58666d061475276dfb3590b9042c
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-11-22 10:03:02 +0100

    Dodana klasa Student

---
+++ project/Student.java
@@ -0,0 +1,35 @@
+public class Student {
+    private String ime;
+    private String prezime;
+    private String brojIndeksa;
+
+    public Student(String ime, String prezime, String brojIndeksa) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.brojIndeksa = brojIndeksa;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getBrojIndeksa() {
+        return brojIndeksa;
+    }
+
+    public void setBrojIndeksa(String brojIndeksa) {
+        this.brojIndeksa = brojIndeksa;
+    }
+}
