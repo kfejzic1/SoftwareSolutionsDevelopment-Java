07af48eb6379ba83dbb95338ba8acb56aa5114b9
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-09 10:34:55 +0100

    Dodana apstraktna klasa Osoba

---
+++ src/Osoba.java
@@ -0,0 +1,24 @@
+public abstract class Osoba {
+    String ime, prezime;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public Osoba(String ime, String prezime) {
+        this.ime = ime;
+        this.prezime = prezime;
+    }
+}
