9fc760da896f2331d1a6447e5ea413681a2ba4da
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-09 10:48:30 +0100

    Dodana klasa Predmet

---
+++ src/Predmet.java
@@ -0,0 +1,54 @@
+public class Predmet {
+    private String naziv;
+    private int ects, brojCasova;
+    private boolean daLiJeIzborni;
+    private Profesor profesor;
+
+    public Predmet(String naziv, int ects, int brojCasova, boolean daLiJeIzborni, Profesor profesor) {
+        this.naziv = naziv;
+        this.ects = ects;
+        this.brojCasova = brojCasova;
+        this.daLiJeIzborni = daLiJeIzborni;
+        this.profesor = profesor;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getEcts() {
+        return ects;
+    }
+
+    public void setEcts(int ects) {
+        this.ects = ects;
+    }
+
+    public int getBrojCasova() {
+        return brojCasova;
+    }
+
+    public void setBrojCasova(int brojCasova) {
+        this.brojCasova = brojCasova;
+    }
+
+    public boolean isDaLiJeIzborni() {
+        return daLiJeIzborni;
+    }
+
+    public void setDaLiJeIzborni(boolean daLiJeIzborni) {
+        this.daLiJeIzborni = daLiJeIzborni;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+}
