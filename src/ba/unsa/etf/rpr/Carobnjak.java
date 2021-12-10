4755ab0f3a1cbadbe7e4393ef42738efb38af68c
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-10 15:02:10 +0100

    Dodana metoda konzumirajSve

--- src/ba/unsa/etf/rpr/Carobnjak.java
+++ src/ba/unsa/etf/rpr/Carobnjak.java
@@ -82,6 +82,16 @@ public class Carobnjak {
     }
 
     String konzumirajSve(){
-        return "";
+        String s = "";
+        for(Eliksir e: eliksiri){
+            s+=e.konzumiraj();
+        }
+        for(Biljka b: biljke){
+            s+=b.konzumiraj();
+        }
+
+        eliksiri.clear();
+        biljke.clear();
+        return s;
     }
 }
