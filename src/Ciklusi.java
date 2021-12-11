fba700a29962071f409f14029033339c2e048ca2
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-11 19:24:42 +0100

    Dodana metoda toString u enum Ciklusi

--- src/Ciklusi.java
+++ src/Ciklusi.java
@@ -1,3 +1,13 @@
 public enum Ciklusi {
-    Bachelor, Master, PhD
+    Bachelor("Bachelor"), Master("Master"), PhD("PhD");
+
+    private final String name;
+
+    Ciklusi(String s){
+        name = s;
+    }
+
+    public String toString(){
+        return this.name;
+    }
 }
