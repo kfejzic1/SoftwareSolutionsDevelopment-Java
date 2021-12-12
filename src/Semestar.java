31262fbdd101142d849d8bf0111942216f5eb3b1
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-12 11:06:23 +0100

    Dodane metode equals, hashcode i pomocna metoda updateNormeProfesora.

--- src/Semestar.java
+++ src/Semestar.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 public class Semestar {
     private int redniBroj;
@@ -7,11 +10,18 @@ public class Semestar {
     private ArrayList<Predmet> upisaniIzborniPredmeti;
     private Ciklusi ciklus;
 
+    private void updateNormeProfesora(Predmet predmet){
+        //Vrši se nakon upisa semestra
+        Profesor profesor = predmet.getProfesor();
+        profesor.setNorma(profesor.getNorma()+ predmet.getBrojCasova());
+    }
+
     public Semestar(){
         this.obavezniPredmeti = new ArrayList<>();
         this.izborniPredmeti = new ArrayList<>();
         this.upisaniIzborniPredmeti = new ArrayList<>();
     }
+
     public Semestar(int redniBroj, Ciklusi ciklus) {
         this.redniBroj = redniBroj;
         this.ciklus = ciklus;
@@ -26,6 +36,11 @@ public class Semestar {
         this.izborniPredmeti = izborniPredmeti;
         this.upisaniIzborniPredmeti = upisaniIzborniPredmeti;
         this.ciklus = ciklus;
+
+        for(Predmet p : obavezniPredmeti)
+            updateNormeProfesora(p);
+        for(Predmet p : upisaniIzborniPredmeti)
+            updateNormeProfesora(p);
     }
 
     public int getRedniBroj() {
@@ -69,7 +84,11 @@ public class Semestar {
     }
 
     public void dodajObavezniPredmet(Predmet predmet){
+        if(obavezniPredmeti.contains(predmet))
+            throw new IllegalArgumentException("Predmet se već nalazi u semestru!");
+
         this.obavezniPredmeti.add(predmet);
+        updateNormeProfesora(predmet);
     }
 
     public void dodajIzborniPredmet(Predmet predmet){
@@ -95,5 +114,48 @@ public class Semestar {
             throw new IllegalArgumentException("U semestru nije moguće imati više od 30 ECTS poena!");
 
         upisaniIzborniPredmeti.add(predmet);
+        updateNormeProfesora(predmet);
+    }
+
+    public boolean daLiProfesorPredajePredmet(Profesor profesor){
+        for(Predmet p : this.obavezniPredmeti){
+            if(p.getProfesor().equals(profesor))
+                return true;
+        }
+        for(Predmet p : this.upisaniIzborniPredmeti){
+            if(p.getProfesor().equals(profesor))
+                return true;
+        }
+
+        return false;
+    }
+
+    public Set<Profesor> dajProfesore(){
+        Set<Profesor> profesori = new HashSet<>();
+
+        for(Predmet p : obavezniPredmeti)
+            profesori.add(p.getProfesor());
+        for(Predmet p : upisaniIzborniPredmeti)
+            profesori.add(p.getProfesor());
+
+        return profesori;
+    }
+
+    public ArrayList<Predmet> dajSvePredmete(){
+        ArrayList<Predmet> predmeti = obavezniPredmeti;
+        predmeti.addAll(upisaniIzborniPredmeti); //Dodaje sve predmete iz upisanih u jednu listu
+        return predmeti;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Semestar semestar)) return false;
+        return redniBroj == semestar.redniBroj && ciklus == semestar.ciklus;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(redniBroj, ciklus);
     }
 }
