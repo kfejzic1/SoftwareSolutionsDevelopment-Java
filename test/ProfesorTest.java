194bea6c3f9a359135048893103dee523c132a05
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-12 11:18:13 +0100

    Dodan test za klasu Profesor

---
+++ test/ProfesorTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfesorTest {
+
+    @Test
+    void testEquals() {
+        Profesor p1 = new Profesor("Vedran", "Ljubović");
+        Profesor p2 = new Profesor("Željko", "Jurić", 130);
+        Profesor p3 = new Profesor("Vedran", "Ljubović", 140);
+
+        assertEquals(p1,p3);
+        assertNotEquals(p1,p2);
+    }
+
+    @Test
+    void testToString() {
+        Profesor p1 = new Profesor("Vedran", "Ljubović", 130);
+        assertEquals(p1.toString(), "Vedran Ljubović, norma: 130");
+    }
+}
