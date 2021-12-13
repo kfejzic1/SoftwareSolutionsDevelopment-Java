e8d565eac40b948c9bfbee75993fde956c326959
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-14 00:23:56 +0100

    U klasi Student dodani atributi indeks, ocjene, upisaniPredmeti i upisaniSemestar. Dodane metode upisiSemestar, upisiOcjenuIzPredmeta, dajOcjenuIzPredmeta, dajUkupanBrojECTSPoena, upisiIzborniPredmet i dajPrepisOcjena (sve iz prethodne klase index).

--- src/Student.java
+++ src/Student.java
@@ -1,26 +1,96 @@
-public class Student extends Osoba{
-    Indeks indeks;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-    public Student(String ime, String prezime) {
-        super(ime, prezime);
-        this.indeks = null;
+public class Student extends Osoba {
+    private String indeks;
+    private Map<Predmet, Integer> ocjene;
+    private ArrayList<Predmet> upisaniPredmeti;
+    private Semestar upisaniSemestar;
+
+    public ArrayList<Predmet> getUpisaniPredmeti() {
+        return upisaniPredmeti;
     }
 
-    public Student(String ime, String prezime, Indeks indeks){
-        super(ime, prezime);
-        this.indeks = indeks;
+    public Semestar getUpisaniSemestar() {
+        return upisaniSemestar;
     }
 
-    public Indeks getIndeks(){
+    public Student(String ime, String prezime, String brojIndeksa) {
+        super(ime, prezime);
+        this.indeks = brojIndeksa;
+        this.ocjene = new HashMap<>();
+        this.upisaniPredmeti = new ArrayList<>();
+    }
+
+    public String getIndeks() {
         return this.indeks;
     }
 
-    public void setIndeks(Indeks indeks){
+    public void setIndeks(String indeks) {
         this.indeks = indeks;
+    }
+
+    public void upisiSemestar(Semestar semestar) { //Upisuje određeni semestar i odmah upisuje obavezne predmete
+        if (!Objects.isNull(this.upisaniSemestar))
+            throw new IllegalArgumentException("Student je vec upisan na neki od semestara!");
+        this.upisaniSemestar = semestar;
+
+        upisaniPredmeti.addAll(semestar.getObavezniPredmeti());
+    }
+
+    public void upisiOcjenuIzPredmeta(Predmet predmet, int ocjena) {
+        if (ocjene.containsKey(predmet))
+            throw new IllegalArgumentException("Ocjena iz predmeta je vec upisana!");
+
+        ocjene.put(predmet, ocjena);
+    }
+
+    public int dajOcjenuIzPredmeta(Predmet predmet) {
+        if (!this.upisaniPredmeti.contains(predmet))
+            throw new IllegalArgumentException("Student ne pohađa taj predmet!");
+        if (Objects.isNull(this.ocjene.get(predmet)))
+            return 5;
+
+        return this.ocjene.get(predmet);
+    }
+
+    public int dajUkupanBrojECTSPoena() { //Koristiti za upis studenta na fakultet (nije moguce pokrenuti nastavu ako nema 30 ects poena)
+        int ects = 0;
+        for (Predmet p : upisaniPredmeti)
+            ects += p.getEcts();
+
+        return ects;
+    }
+
+    public void upisiIzborniPredmet(Predmet predmet) {
+        if (!this.upisaniSemestar.getIzborniPredmeti().contains(predmet))
+            throw new IllegalArgumentException("Predmet nije izborni u upisanom semestru!");
+        if (this.upisaniPredmeti.contains(predmet))
+            throw new IllegalArgumentException("Student je već upisan na ovaj predmet!");
+        if (dajUkupanBrojECTSPoena() + predmet.getEcts() > 30)
+            throw new IllegalArgumentException("Nije moguće imati više od 30 ECTS poena po semestru!");
+
+        upisaniPredmeti.add(predmet);
+        if (!predmet.daLiJeUpisan()) {     //Potrebno je odraditi update norme profesora jer se predmet prvi put upisuje
+            predmet.getProfesor().setNorma(predmet.getProfesor().getNorma() + predmet.getBrojCasova());
+            predmet.setDaLiJeUpisan(true);
+        }
+    }
+
+    public String dajPrepisOcjena() {
+        String s = "";
+
+        for (Predmet predmet : this.upisaniPredmeti) {
+            s += predmet.getNaziv() + " (" + predmet.getEcts() + " ECTS) - " + dajOcjenuIzPredmeta(predmet) + "\n";
+        }
+
+        return s;
     }
 
     @Override
     public String toString() {
-        return this.ime + " " + this.prezime + "\nBroj indeksa: " + this.indeks.getBrojIndeksa();
+        return this.ime + " " + this.prezime + "\nBroj indeksa: " + this.indeks;
     }
 }
