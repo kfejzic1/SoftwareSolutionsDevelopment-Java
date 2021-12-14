88f041d5849c73332179bbed3871153eb52be564
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-14 13:22:38 +0100

    Izvrsen code cleanup u klasi Osoba

--- src/ba/unsa/etf/rpr/Osoba.java
+++ src/ba/unsa/etf/rpr/Osoba.java
@@ -1,22 +1,15 @@
 package ba.unsa.etf.rpr;
 
 public abstract class Osoba {
-    private String ime, prezime;
+    private final String ime;
+    private final String prezime;
 
     public String getIme() {
         return ime;
     }
 
-    public void setIme(String ime) {
-        this.ime = ime;
-    }
-
     public String getPrezime() {
         return prezime;
-    }
-
-    public void setPrezime(String prezime) {
-        this.prezime = prezime;
     }
 
     public Osoba(String ime, String prezime) {
