13ee38d259b86691f2f2309b6ad26c8606c6f9c8
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-18 20:17:33 +0100

    Kreiran korisnički interfejs

---
+++ src/ba/unsa/etf/rpr/KontrolerForma.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr;
+
+public class KontrolerForma {
+
+//    @FXML
+//    public TextField fldText;
+//    fldText.setMouseTransparent(true); Dodati u initialize()
+//    fldText.setFocusTraversable(false);
+
+}
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,7 +1,29 @@
 package ba.unsa.etf.rpr;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/zadaca3.fxml"));
+        primaryStage.setTitle("Studenti");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.show();
+
+        primaryStage.setMinWidth(590);
+        primaryStage.setMinHeight(450);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Sretno sa rješavanjem zadaće 3!");
+        launch(args);
     }
 }
