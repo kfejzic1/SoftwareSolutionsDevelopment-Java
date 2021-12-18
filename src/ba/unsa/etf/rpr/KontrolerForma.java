98972e0c7efe19eed1e91919092dfebce248bdf1
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-18 20:39:22 +0100

    Jednosmjerno povezivanje button-a i txtField-a

--- src/ba/unsa/etf/rpr/KontrolerForma.java
+++ src/ba/unsa/etf/rpr/KontrolerForma.java
@@ -1,10 +1,30 @@
 package ba.unsa.etf.rpr;
 
-public class KontrolerForma {
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
-//    @FXML
-//    public TextField fldText;
-//    fldText.setMouseTransparent(true); Dodati u initialize()
-//    fldText.setFocusTraversable(false);
+public class KontrolerForma {
+    private SimpleStringProperty idNumber = new SimpleStringProperty("");
+
+    public String getIdNumber() {
+        return idNumber.get();
+    }
+
+    public SimpleStringProperty idNumberProperty() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber.set(idNumber);
+    }
+
+    @FXML
+    public void getNumber(ActionEvent actionEvent) {
+        String number = ((Button)actionEvent.getSource()).getText(); //Uzima broj kao text buttona
+
+        this.idNumber.set(this.idNumber.get()+number);
+    }
 
 }
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,13 +1,10 @@
 package ba.unsa.etf.rpr;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
