4ae99b5b42c3b82a11ae376ca9c7231345d4ca56
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-18 16:34:58 +0100

    Implementirana funkcionalnost button-a Kraj

--- src/ba/unsa/etf/rpr/tutorijal06/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Controller.java
@@ -2,92 +2,32 @@ package ba.unsa.etf.rpr.tutorijal06;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+import java.util.Optional;
 
 public class Controller {
-    boolean daLiJeDoubleInteger(Double broj){
-        return (broj == Math.floor(broj)) && !Double.isInfinite(broj);
-    }
+    @FXML
+    public Button btnKraj;
 
     @FXML
-    private Label display;
-    private boolean start = true;   //Postaje true ako je kliknuta bilo koja tipka
-    private boolean decimalKlik = false;    //Postaje true ako je kliknut decimalni zarez
-    private String operacija;
-    private Double prviBroj;
+    public void kraj(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Potvrda");
+        alert.setHeaderText("Izlaz iz programa");
+        alert.setContentText("Da li ste sigurni da želite zatvoriti program?");
 
-    @FXML
-    public void dajBroj(ActionEvent actionEvent) {
-        if(start){
-            display.setText("");
-            start = false;
-        }
-
-        String broj = ((Button)actionEvent.getSource()).getText();
-
-        if(display.getText().equals("0") && broj.equals("0")){
-            start = false;
-            return;
-        }
-
-        //Ako se unese broj razlicit od 0, a 0 je na ekranu, potrebno je obrisati ekran
-        if(!broj.equals("0") && display.getText().equals("0"))
-            display.setText("");
-
-        display.setText(display.getText()+broj);
-    }
-
-    @FXML
-    public void decimalnaTacka(ActionEvent actionEvent) {
-        if(!decimalKlik && !start){
-            String decimalniDioBroja = ((Button)actionEvent.getSource()).getText();
-            display.setText(display.getText()+decimalniDioBroja);
-            decimalKlik = true;
-        }
-    }
-
-    @FXML
-    public void dajOperaciju(ActionEvent actionEvent) {
-        operacija = ((Button)actionEvent.getSource()).getText();
-        prviBroj = Double.parseDouble(display.getText());
-        display.setText("0");
-        decimalKlik = false;
-        start = true;
-    }
-
-    @FXML
-    public void znakJednakosti(ActionEvent actionEvent) {
-        Double drugiBroj = Double.parseDouble(display.getText());
-        double rezultat = 0.;
-
-        switch (operacija){
-            case "+":
-                rezultat = prviBroj+drugiBroj;
-                break;
-            case "-":
-                rezultat = prviBroj-drugiBroj;
-                break;
-            case "x":
-                rezultat = prviBroj*drugiBroj;
-                break;
-            case "/":
-                rezultat = prviBroj/drugiBroj;
-                break;
-            case "%":
-                if(daLiJeDoubleInteger(prviBroj) && daLiJeDoubleInteger(drugiBroj)){
-                    rezultat = (prviBroj.intValue() % drugiBroj.intValue());
-                    display.setText(Integer.toString((int) rezultat));
-                }else
-                    display.setText("Nemoguće");
-                break;
-            default:
-        }
-        start = true;
-        decimalKlik = false;
-
-        if(!operacija.equals("%")){
-            display.setText(Double.toString(rezultat));
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get() == ButtonType.OK) {
+            //Zatvori program
+            Stage stage  = (Stage) btnKraj.getScene().getWindow();
+            stage.close();
+        } else {
+            //Zatvori alert
+            alert.close();
         }
     }
 }
