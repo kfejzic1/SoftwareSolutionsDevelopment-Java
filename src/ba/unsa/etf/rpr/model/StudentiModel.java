3ca0efebaf70f0d1bd5c11f50749420d0d311dc9
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-20 10:40:10 +0100

    Ispravljena klasa StudentiModel

--- src/ba/unsa/etf/rpr/model/StudentiModel.java
+++ src/ba/unsa/etf/rpr/model/StudentiModel.java
@@ -1,25 +1,33 @@
 package ba.unsa.etf.rpr.model;
 
-import ba.unsa.etf.rpr.beans.Student;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class StudentiModel {
-    private ObservableList<Student> studenti = FXCollections.observableArrayList();
+    private ObservableList<String> studenti = FXCollections.observableArrayList();
 
-    public ObservableList<Student> getStudenti() {
+    public ObservableList<String> getStudenti() {
         return studenti;
     }
 
-    public void setStudenti(ObservableList<Student> studenti) {
+    public void setStudenti(ObservableList<String> studenti) {
         this.studenti = studenti;
     }
 
     public void napuni() {
-        studenti.add(new Student("Kenan Fejzić"));
-        studenti.add(new Student("Suljo Suljić"));
-        studenti.add(new Student("Hasan Hasanović"));
-        studenti.add(new Student("Mujo Mujić"));
-        studenti.add(new Student("Neko Nekić"));
+        studenti.add("Student1");
+        studenti.add("Student2");
+        studenti.add("Student3");
+        studenti.add("Student4");
+        studenti.add("Student5");
+        studenti.add("Student6");
+        studenti.add("Student7");
+        studenti.add("Student8");
+        studenti.add("Student9");
+        studenti.add("Student10");
+        studenti.add("Student11");
+        studenti.add("Student12");
+        studenti.add("Student13");
+        studenti.add("Student14");
     }
 }
