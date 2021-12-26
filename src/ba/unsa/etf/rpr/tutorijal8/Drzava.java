abff68272819228f76fd7ec5d4e694d04b696104
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-26 22:48:07 +0100

    Kreirana klasa Drzava

---
+++ src/ba/unsa/etf/rpr/tutorijal8/Drzava.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.tutorijal8;
+
+public class Drzava {
+    private String naziv;
+    private int brojStanovnika;
+    private double povrsina;
+    private String jedinicaZaPovrsinu;
+    private Grad glavniGrad;
+}
