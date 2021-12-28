226b8c8a1557c7058e7110b21f08e72a46b5bfd7
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-28 10:24:53 +0100

    Implementirana metoda napuni, ispravljen konstruktor

--- src/ba/unsa/etf/rpr/t7/KorisniciModel.java
+++ src/ba/unsa/etf/rpr/t7/KorisniciModel.java
@@ -4,11 +4,50 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.Scanner;
+
 public class KorisniciModel {
     private ObservableList<Korisnik> korisnici = FXCollections.observableArrayList();
     private SimpleObjectProperty<Korisnik> trenutniKorisnik = new SimpleObjectProperty<>();
 
+    private static KorisniciModel instanca = null;  //možda nije potrebno
+
+    private Connection conn;
+    private PreparedStatement stmt;
+
     public KorisniciModel() {
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:korisnici.db");
+        } catch(SQLException e) {
+            System.out.println("Neuspješno čitanje iz baze: " + e.getMessage());
+        }
+        if (trenutniKorisnik == null) trenutniKorisnik = new SimpleObjectProperty<>();
+    }
+
+    public void regenerisiBazu() {
+        Scanner ulaz = null;
+        try {
+            ulaz = new Scanner(new FileInputStream("korisnik.sql"));
+            String sqlUpit = "";
+            while (ulaz.hasNext()) {
+                sqlUpit += ulaz.nextLine();
+                if ( sqlUpit.length() > 1 && sqlUpit.charAt( sqlUpit.length()-1 ) == ';') {
+                    try {
+                        Statement stmt = conn.createStatement();
+                        stmt.execute(sqlUpit);
+                        sqlUpit = "";
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            ulaz.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Ne postoji SQL datoteka... nastavljam sa praznom bazom");
+        }
     }
 
     public void napuni() {
@@ -16,11 +55,29 @@ public class KorisniciModel {
         // Na taj način se metoda napuni() može pozivati više puta u testovima
         korisnici.clear();
 
-        korisnici.add(new Korisnik("Vedran", "Ljubović", "vljubovic@etf.unsa.ba", "vedranlj", "test"));
-        korisnici.add(new Korisnik("Amra", "Delić", "adelic@etf.unsa.ba", "amrad", "test"));
-        korisnici.add(new Korisnik("Tarik", "Sijerčić", "tsijercic1@etf.unsa.ba", "tariks", "test"));
-        korisnici.add(new Korisnik("Rijad", "Fejzić", "rfejzic1@etf.unsa.ba", "rijadf", "test"));
-        trenutniKorisnik.set(null);
+        try {
+            stmt = conn.prepareStatement("SELECT Ime, Prezime, Email, Username, Password FROM korisnik");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Korisnik k = new Korisnik(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+                korisnici.add(k);
+                if (trenutniKorisnik == null) trenutniKorisnik = new SimpleObjectProperty<>(k);
+            }
+        } catch(SQLException e) {
+            regenerisiBazu();
+            try {
+                stmt = conn.prepareStatement("SELECT Ime, Prezime, Email, Username, Password FROM korisnik");
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()) {
+                    Korisnik k = new Korisnik(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+                    korisnici.add(k);
+                    if (trenutniKorisnik == null) trenutniKorisnik = new SimpleObjectProperty<>(k);
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (trenutniKorisnik == null) trenutniKorisnik = new SimpleObjectProperty<>();
     }
 
     public void vratiNaDefault() {
