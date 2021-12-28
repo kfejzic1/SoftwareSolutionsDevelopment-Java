93b36b307d9f5297cda5238b25ed63380e41afac
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-28 10:24:29 +0100

    Dodana baza korisnik.db i dump file korisnik.sql

---
+++ korisnik.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS "korisnik" (
+	"Ime"	TEXT,
+	"Prezime"	TEXT,
+	"Email"	TEXT,
+	"Username"	TEXT,
+	"Password"	TEXT
+);
+INSERT INTO "korisnik" VALUES ('Vedran','Ljubović','vljubovic@etf.unsa.ba','vedranlj','test');
+INSERT INTO "korisnik" VALUES ('Amra','Delic','adelic@etf.unsa.ba','amrad','test');
+INSERT INTO "korisnik" VALUES ('Tarik','Sijerčić','tsijercic1@etf.unsa.ba','tariks','test');
+INSERT INTO "korisnik" VALUES ('Rijad','Fejzić','rfejzic1@etf.unsa.ba','rijadf','test');
