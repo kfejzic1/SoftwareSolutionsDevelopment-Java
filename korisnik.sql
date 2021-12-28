20991a362e23543355c641478481a23ec6592869
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-28 11:42:31 +0100

    Modify tabele korisnik (dodan id)

--- korisnik.sql
+++ korisnik.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS "korisnik" (
+    "Id"	INTEGER,
 	"Ime"	TEXT,
 	"Prezime"	TEXT,
 	"Email"	TEXT,
 	"Username"	TEXT,
-	"Password"	TEXT
+	"Password"	TEXT,
+	PRIMARY KEY("Id")
 );
-INSERT INTO "korisnik" VALUES ('Vedran','Ljubović','vljubovic@etf.unsa.ba','vedranlj','test');
-INSERT INTO "korisnik" VALUES ('Amra','Delić','adelic@etf.unsa.ba','amrad','test');
-INSERT INTO "korisnik" VALUES ('Tarik','Sijerčić','tsijercic1@etf.unsa.ba','tariks','test');
-INSERT INTO "korisnik" VALUES ('Rijad','Fejzić','rfejzic1@etf.unsa.ba','rijadf','test');
+INSERT INTO "korisnik" VALUES ('1','Vedran','Ljubović','vljubovic@etf.unsa.ba','vedranlj','test');
+INSERT INTO "korisnik" VALUES ('2','Amra','Delić','adelic@etf.unsa.ba','amrad','test');
+INSERT INTO "korisnik" VALUES ('3','Tarik','Sijerčić','tsijercic1@etf.unsa.ba','tariks','test');
+INSERT INTO "korisnik" VALUES ('4','Rijad','Fejzić','rfejzic1@etf.unsa.ba','rijadf','test');
