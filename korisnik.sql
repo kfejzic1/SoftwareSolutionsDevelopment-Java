814fa4bb7692d85eb2d46a89126c363a3e8277d8
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-28 10:26:52 +0100

    Ispravljen dumb file

--- korisnik.sql
+++ korisnik.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS "korisnik" (
 	"Password"	TEXT
 );
 INSERT INTO "korisnik" VALUES ('Vedran','Ljubović','vljubovic@etf.unsa.ba','vedranlj','test');
-INSERT INTO "korisnik" VALUES ('Amra','Delic','adelic@etf.unsa.ba','amrad','test');
+INSERT INTO "korisnik" VALUES ('Amra','Delić','adelic@etf.unsa.ba','amrad','test');
 INSERT INTO "korisnik" VALUES ('Tarik','Sijerčić','tsijercic1@etf.unsa.ba','tariks','test');
 INSERT INTO "korisnik" VALUES ('Rijad','Fejzić','rfejzic1@etf.unsa.ba','rijadf','test');
