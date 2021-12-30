3e724a4e08d5ad4f76ebdd4edae3e5e6897475e7
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2021-12-30 15:57:34 +0100

    Dodana klasa Drzava

---
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr;
+
+public class Drzava {
+    int id;
+    String naziv;
+    Grad glavniGrad;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
+}
