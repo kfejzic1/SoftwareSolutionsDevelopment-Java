c33a7333da9f3a7d94b87116343549dc456352ae
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2022-01-06 13:51:22 +0100

    Dodani ResourceBundle u testove

--- test/ba/unsa/etf/rpr/t7/Zadatak2Test.java
+++ test/ba/unsa/etf/rpr/t7/Zadatak2Test.java
@@ -16,6 +16,7 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
 import java.io.File;
+import java.util.ResourceBundle;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,7 +30,8 @@ public class Zadatak2Test {
         model = new KorisniciModel();
         KorisnikController ctrl = new KorisnikController(model);
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/korisnici.fxml"));
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/korisnici.fxml"), bundle);
         loader.setController(ctrl);
         Parent root = loader.load();
         stage.setTitle("Korisnici");
