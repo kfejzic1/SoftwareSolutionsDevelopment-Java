0c640c3ada3b36aa94737a19de796b2991f2c6f4
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2022-01-09 23:51:08 +0100

    Implementirana GlavnaController

---
+++ src/ba/unsa/etf/rpr/GlavnaController.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+public class GlavnaController {
+
+    public void dodajGradAction(ActionEvent actionEvent) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/drzava.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage  = new Stage();
+        stage.setTitle("Država");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public void dodajDrzavuAction(ActionEvent actionEvent) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/grad.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage  = new Stage();
+        stage.setTitle("Grad");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+}
