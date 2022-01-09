e7a2ac2a1847a5a4aa36a3efdc36d9c21271109a
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2022-01-10 00:08:03 +0100

    Dodani handleri za btnIzmijeniGrad

--- src/ba/unsa/etf/rpr/GlavnaController.java
+++ src/ba/unsa/etf/rpr/GlavnaController.java
@@ -12,6 +12,20 @@ import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class GlavnaController {
 
+    public void dodajDrzavuAction(ActionEvent actionEvent) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/grad.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage  = new Stage();
+        stage.setTitle("Grad");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+
     public void dodajGradAction(ActionEvent actionEvent) {
         Parent root = null;
         try {
@@ -26,15 +40,15 @@ public class GlavnaController {
         stage.show();
     }
 
-    public void dodajDrzavuAction(ActionEvent actionEvent) {
+    public void izmijeniGradAction(ActionEvent actionEvent) {
         Parent root = null;
         try {
-            root = FXMLLoader.load(getClass().getResource("/fxml/grad.fxml"));
+            root = FXMLLoader.load(getClass().getResource("/fxml/drzava.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
         Stage stage  = new Stage();
-        stage.setTitle("Grad");
+        stage.setTitle("Država");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
         stage.show();
