16825a4b84dcc5e269d669036d3a8cf9c7a40d0b
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2022-01-10 00:05:42 +0100

    Implementiran DrzavaController

---
+++ src/ba/unsa/etf/rpr/DrzavaController.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class DrzavaController {
+
+    public TextField fieldNaziv;
+
+
+    public void btnOkAction(ActionEvent actionEvent) {
+        if(fieldNaziv.getText().isEmpty()) {
+            fieldNaziv.getStyleClass().removeAll("poljeValidno");
+            fieldNaziv.getStyleClass().add("poljeNijeValidno");
+        } else {
+            fieldNaziv.getStyleClass().removeAll("poljeNijeValidno");
+            fieldNaziv.getStyleClass().add("poljeValidno");
+        }
+        System.out.println("Kliknuto!");
+    }
+
+    public void btnCancelAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) fieldNaziv.getScene().getWindow();
+        stage.close();
+    }
+}
