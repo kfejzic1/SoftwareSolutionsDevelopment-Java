841d6962b90b350035a3b9493f36be546aa7fd84
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2022-01-09 21:25:54 +0100

    Ispravljeni podaci

--- baza.sql
+++ baza.sql
@@ -14,12 +14,12 @@ CREATE TABLE IF NOT EXISTS "grad" (
 	FOREIGN KEY("drzava") REFERENCES "drzava"("id"),
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
-INSERT INTO "drzava" VALUES (1,'Engleska',1);
+INSERT INTO "drzava" VALUES (1,'Velika Britanija',1);
 INSERT INTO "drzava" VALUES (2,'Francuska',2);
 INSERT INTO "drzava" VALUES (3,'Austrija',3);
-INSERT INTO "grad" VALUES (1,'London',8982000,1);
-INSERT INTO "grad" VALUES (2,'Pariz',2161000,2);
-INSERT INTO "grad" VALUES (3,'Beč',1897000,3);
-INSERT INTO "grad" VALUES (4,'Manchester',553230,2);
-INSERT INTO "grad" VALUES (5,'Graz',283869,3);
+INSERT INTO "grad" VALUES (1,'London',8825000,1);
+INSERT INTO "grad" VALUES (2,'Pariz',2206488,2);
+INSERT INTO "grad" VALUES (3,'Beč',1899055,3);
+INSERT INTO "grad" VALUES (4,'Manchester',545500,1);
+INSERT INTO "grad" VALUES (5,'Graz',280200,3);
 COMMIT;
