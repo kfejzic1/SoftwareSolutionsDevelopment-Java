5a8d15cfe88bc63f1957d2719e5859331647183d
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2022-01-10 00:25:05 +0100

    Dependency injection princip

--- src/ba/unsa/etf/rpr/GlavnaController.java
+++ src/ba/unsa/etf/rpr/GlavnaController.java
@@ -12,13 +12,12 @@ import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class GlavnaController {
 
-    public void dodajDrzavuAction(ActionEvent actionEvent) {
-        Parent root = null;
-        try {
-            root = FXMLLoader.load(getClass().getResource("/fxml/drzava.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void dodajDrzavuAction(ActionEvent actionEvent) throws IOException {
+        DrzavaController ctrl = new DrzavaController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/drzava.fxml"));
+        loader.setController(ctrl);
+        Parent root = loader.load();
+
         Stage stage  = new Stage();
         stage.setTitle("Država");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
@@ -26,13 +25,12 @@ public class GlavnaController {
         stage.show();
     }
 
-    public void dodajGradAction(ActionEvent actionEvent) {
-        Parent root = null;
-        try {
-            root = FXMLLoader.load(getClass().getResource("/fxml/grad.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void dodajGradAction(ActionEvent actionEvent) throws IOException {
+        GradController ctrl = new GradController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/grad.fxml"));
+        loader.setController(ctrl);
+        Parent root = loader.load();
+
         Stage stage  = new Stage();
         stage.setTitle("Grad");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
@@ -40,13 +38,12 @@ public class GlavnaController {
         stage.show();
     }
 
-    public void izmijeniGradAction(ActionEvent actionEvent) {
-        Parent root = null;
-        try {
-            root = FXMLLoader.load(getClass().getResource("/fxml/grad.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void izmijeniGradAction(ActionEvent actionEvent) throws IOException {
+        GradController ctrl = new GradController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/grad.fxml"));
+        loader.setController(ctrl);
+        Parent root = loader.load();
+
         Stage stage  = new Stage();
         stage.setTitle("Grad");
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
