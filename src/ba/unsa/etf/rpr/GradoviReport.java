25eeeba3fb8acbf683d8bbe0b1ed83bb8adf48ea
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2022-01-17 15:04:50 +0100

    Dodana report klasa GradoviReport

---
+++ src/ba/unsa/etf/rpr/GradoviReport.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.swing.JRViewer;
+import net.sf.jasperreports.view.JasperViewer;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GradoviReport extends JFrame {
+    public void showReport(Connection conn) throws JRException {
+        String reportSrcFile = getClass().getResource("/reports/gradovi.jrxml").getFile();
+        String reportsDir = getClass().getResource("/reports/").getFile();
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
+        // Fields for resources path
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("reportsDirPath", reportsDir);
+        ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+        list.add(parameters);
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, conn);
+        JasperViewer viewer = new JasperViewer(print, false);
+        viewer.setVisible(true);
+    }
+}
