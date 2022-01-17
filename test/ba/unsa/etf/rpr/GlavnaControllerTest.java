23c48439ba75a6b1dd64a6ef9125845fcf5afd9c
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2022-01-17 20:39:34 +0100

    Dodan bundle u loader

--- test/ba/unsa/etf/rpr/GlavnaControllerTest.java
+++ test/ba/unsa/etf/rpr/GlavnaControllerTest.java
@@ -15,6 +15,7 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,7 +28,8 @@ class GlavnaControllerTest {
 
     @Start
     public void start (Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/glavna.fxml"));
+        ResourceBundle bundle = ResourceBundle.getBundle("Translation");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/glavna.fxml"), bundle);
         ctrl = new GlavnaController();
         loader.setController(ctrl);
         Parent root = loader.load();
