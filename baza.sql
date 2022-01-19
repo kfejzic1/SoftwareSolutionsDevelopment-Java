a98432a84252412f2423bb21058448549b7a390c
Kenan Fejzic <kfejzic1@etf.unsa.ba>
2022-01-19 15:23:57 +0100

    Dodana kolona postanski_broj

--- baza.sql
+++ baza.sql
@@ -12,15 +12,16 @@ CREATE TABLE IF NOT EXISTS "grad" (
 	"broj_stanovnika"	INTEGER,
 	"drzava"	INTEGER,
 	"slika" TEXT,
+	"postanski_broj"    INTEGER,
 	FOREIGN KEY("drzava") REFERENCES "drzava"("id"),
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 INSERT INTO "drzava" VALUES (1,'Velika Britanija',1);
 INSERT INTO "drzava" VALUES (2,'Francuska',2);
 INSERT INTO "drzava" VALUES (3,'Austrija',3);
-INSERT INTO "grad" VALUES (1,'London',8825000,1, '');
-INSERT INTO "grad" VALUES (2,'Pariz',2206488,2, '');
-INSERT INTO "grad" VALUES (3,'Beč',1899055,3, '');
-INSERT INTO "grad" VALUES (4,'Manchester',545500,1, '');
-INSERT INTO "grad" VALUES (5,'Graz',280200,3, '');
+INSERT INTO "grad" VALUES (1,'London',8825000,1, 'resources/images/london.jpg', 31000);
+INSERT INTO "grad" VALUES (2,'Pariz',2206488,2, '', null);
+INSERT INTO "grad" VALUES (3,'Beč',1899055,3, '', null);
+INSERT INTO "grad" VALUES (4,'Manchester',545500,1, '', null);
+INSERT INTO "grad" VALUES (5,'Graz',280200,3, '', null);
 COMMIT;
